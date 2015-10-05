0d0486cf18207b09acb5ee19c5c7d77b44ee7dc3
ginnocen <ginnocen@cern.ch>
2015-10-05 15:02:02 +0200

    fixes

--- getHFMenu.sh
+++ getHFMenu.sh
@@ -7,6 +7,10 @@ sed -i '/process = cms.Process( "TEST" )/a process.load("setup_cff")' hlt_MC_sta
 
 echo 'process.load('\''L1Trigger.L1TCalorimeter.caloConfigStage1HI_cfi'\'')' >> hlt_MC_stage1.py
 
+echo 'process.load("HeavyIonsAnalysis.JetAnalysis.HiGenAnalyzer_cfi")'>> hlt_MC_stage1_TRK2_HIcode.py  
+echo 'process.load("GeneratorInterface.HiGenCommon.HeavyIon_cff")'>> hlt_MC_stage1_TRK2_HIcode.py      
+
+
 #echo 'from L1Trigger.L1TCommon.customsPostLS1 import customiseSimL1EmulatorForPostLS1_HI
 #customiseSimL1EmulatorForPostLS1_HI(process) ' >> hlt_MC_stage1.py
 
@@ -17,17 +21,16 @@ echo 'process.load('\''L1Trigger.L1TCalorimeter.caloConfigStage1HI_cfi'\'')' >> 
 perl -pi -e 's/useHF = cms.untracked.bool/useHF = cms.bool/g' hlt_MC_stage1.py
 #perl -pi -e 's/L1_SingleMu3_BptxAND/L1_ZeroBias/g' hlt_MC_stage1.py
 
-#comment this in case you dont want to correct for beam spot 
-
-echo 'from CondCore.DBCommon.CondDBSetup_cfi import *
-process.beamspot = cms.ESSource("PoolDBESSource",CondDBSetup
+echo '
+from CondCore.DBCommon.CondDBSetup_cfi import *
+process.beamspot = cms.ESSource("PoolDBESSource",CondDBSetup,
                                 toGet = cms.VPSet(cms.PSet( record = cms.string("BeamSpotObjectsRcd"),
-                                                            tag= cms.string("RealisticHICollisions2011_STAR#THI50_mc")
+                                                            tag= cms.string("RealisticHICollisions2011_STARTHI50_mc")
                                                             )),
                                 connect =cms.string("frontier://FrontierProd/CMS_COND_31X_BEAMSPOT")
                                 )
-process.es_prefer_beamspot = cms.ESPrefer("PoolDBESSource","beamspot")' >> hlt_MC_stage1.py
-##############
+process.es_prefer_beamspot = cms.ESPrefer("PoolDBESSource","beamspot")
+'>> hlt_MC_stage1.py
 
 
 echo 'process.load("HLTrigger.HLTanalyzers.HLTBitAnalyser_cfi")' >> hlt_MC_stage1.py
@@ -40,16 +43,6 @@ echo 'process.TFileService = cms.Service("TFileService",
                                    fileName=cms.string("openHLT_HF.root"))' >> hlt_MC_stage1.py
 
 
-echo 'from CondCore.DBCommon.CondDBSetup_cfi import *
-process.beamspot = cms.ESSource("PoolDBESSource",CondDBSetup,
-                                toGet = cms.VPSet(cms.PSet( record = cms.string('\''BeamSpotObjectsRcd'\''),
-                                                            tag= cms.string('\''RealisticHICollisions2011_STARTHI50_mc'\'')
-                                                            )),
-                                connect =cms.string('\''frontier://FrontierProd/CMS_COND_31X_BEAMSPOT'\'')
-                                )
-process.es_prefer_beamspot = cms.ESPrefer("PoolDBESSource","beamspot")' >> hlt_MC_stage1.py
-
-
 echo 'process.load('\''Configuration/StandardSequences/FrontierConditions_GlobalTag_condDBv2_cff'\'')
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 recordOverrides = { ('\''L1RCTParametersRcd'\'', None) : ('\''L1RCTParametersRcd_L1TDevelCollisions_ExtendedScaleFactorsV4_HIDisabledFGHOE'\'', None) }
@@ -58,13 +51,13 @@ process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")' >> hlt_M
 
 ### adding gen particle info & timing
 
-echo '                                                                             
-process.HiGenParticleAna.genParticleSrc = cms.untracked.InputTag("genParticles")    ###TO BE ADDED TO RUN HIGENANALYZER
-process.HiGenParticleAna.stableOnly = cms.untracked.bool(False)                     ###TO BE ADDED TO RUN HIGENANALYZER
-process.ana_step = cms.Path(process.heavyIon*                                       ###TO BE ADDED TO RUN HIGENANALYZER
-      process.HiGenParticleAna                                                      ###TO BE ADDED TO RUN HIGENANALYZER
-)                                                                                   ###TO BE ADDED TO RUN HIGENANALYZER
-'>> hlt_MC_stage1.py
+#echo '                                                                             
+#process.HiGenParticleAna.genParticleSrc = cms.untracked.InputTag("genParticles")    
+#process.HiGenParticleAna.stableOnly = cms.untracked.bool(False)                     
+#process.ana_step = cms.Path(process.heavyIon*                                       
+#      process.HiGenParticleAna                                                      
+#)                                                                                   
+#'>> hlt_MC_stage1.py
 
 echo '
 process.Timing=cms.Service("Timing",
