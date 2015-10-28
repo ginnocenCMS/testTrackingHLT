e0dea3439c0eeb4fb4df23d9f3f1a5553b666571
ginnocen <ginnocen@cern.ch>
2015-10-28 10:57:34 +0100

    local submission area

--- crabConfig.py
+++ crabConfig.py
@@ -6,7 +6,9 @@ config.section_('JobType')
 config.JobType.psetName = 'hlt_MC_stage1.py'
 config.JobType.pluginName = 'Analysis'
 config.JobType.inputFiles = ['rssLimit']
-config.JobType.outputFiles = ['openHLT.root']
+config.JobType.outputFiles = ['openHLT_HF.root']
+config.General.requestName = 'HeavyFlavourHLT_MVA_41'
+config.General.workArea = 'crab_projects'
 config.section_('Data')
 config.Data.inputDataset = '/Pyquen_D0tokaonpion_D0pt15p0_Pthat15_TuneZ2_Unquenched_5020GeV_GENSIM_75x_v2/mnguyen-D0pthat15-RECO_v2-2fbe1a61b2b7a96a0ed51c8b458e4279/USER'
 config.Data.publication = False
