0300d7c0ee7bca6526f379cc542d31c8a0504a97
ginnocen <ginnocen@cern.ch>
2015-10-28 01:40:35 +0100

    d

--- crabConfig.py
+++ crabConfig.py
@@ -3,18 +3,18 @@ config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
 config.section_('JobType')
-config.JobType.psetName = 'hlt_MC_stage1_TRK2_HIcode.py'
+config.JobType.psetName = 'hlt_MC_stage1.py'
 config.JobType.pluginName = 'Analysis'
 config.JobType.inputFiles = ['rssLimit']
 config.JobType.outputFiles = ['openHLT.root']
 config.section_('Data')
-config.Data.inputDataset = '/PyquenUnquenched_Dijet_NcollFilt_pthat80_740pre8_MCHI1_74_V4_GEN-SIM_v3/mnguyen-PyquenUnquenched_Dijet_pthat80_740pre8_MCHI2_74_V3_DIGI-RAW_v2-ee815b27030c232e2e0a7be48a50a463/USER'
+config.Data.inputDataset = '/Pyquen_D0tokaonpion_D0pt15p0_Pthat15_TuneZ2_Unquenched_5020GeV_GENSIM_75x_v2/mnguyen-D0pthat15-RECO_v2-2fbe1a61b2b7a96a0ed51c8b458e4279/USER'
 config.Data.publication = False
 config.Data.unitsPerJob = 100
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'EventAwareLumiBased'
-config.Data.publishDataName = 'Dijet_NcollFilt_pthat80_740pre8_MCHI1_74_V4_SingleTrackHLT'
+config.Data.publishDataName = '/store/user/ginnocen/HeavyFlavourHLT_MVA_41'
 config.section_('User')
 config.section_('Site')
-config.Site.whitelist = ['T2_FR_GRIF_LLR']
+config.Site.whitelist = ['T2_US_MIT']
 config.Site.storageSite = 'T2_US_MIT'
